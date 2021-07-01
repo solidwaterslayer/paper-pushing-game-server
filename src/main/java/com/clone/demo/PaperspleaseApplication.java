c3429b598ba5f8dc2fc90266480bbcbd582f31ad
Michael N Tran <solidwater969@gmail.com>
2021-06-30 21:27:07 -0400

    feature: initial commit

---
+++ src/main/java/com/clone/demo/PaperspleaseApplication.java
@@ -0,0 +1,13 @@
+package com.clone.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaperspleaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaperspleaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/clone/demo/PaperspleaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clone.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaperspleaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
