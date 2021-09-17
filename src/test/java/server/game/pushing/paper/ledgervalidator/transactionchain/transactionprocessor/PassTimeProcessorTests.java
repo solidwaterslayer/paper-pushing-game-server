5ddcb3a2e42c167d210fadf24e345e41c94e0341
Michael Tran <solidwater969@gmail.com>
2021-09-17 13:55:26 -0400

    refactor: extract variables in time travel processor tests
    Squashed commits:
    
    [e2a2602] refactor: close #6 rename commits

--- src/test/java/server/game/pushing/paper/ledgervalidator/transactionchain/transactionprocessor/PassTimeProcessorTests.java
+++ src/test/java/server/game/pushing/paper/ledgervalidator/transactionchain/transactionprocessor/PassTimeProcessorTests.java
@@ -8,19 +8,19 @@ import server.game.pushing.paper.ledgervalidator.bank.account.AccountType;
 import server.game.pushing.paper.ledgervalidator.bank.account.CD;
 import server.game.pushing.paper.ledgervalidator.bank.account.Checking;
 import server.game.pushing.paper.ledgervalidator.bank.account.Savings;
+import server.game.pushing.paper.ledgervalidator.transactionchain.TransactionType;
 
 import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static server.game.pushing.paper.ledgervalidator.bank.Bank.getMaxAPR;
-import static server.game.pushing.paper.ledgervalidator.bank.Bank.getMinInitialCDBalance;
+import static server.game.pushing.paper.ledgervalidator.bank.Bank.*;
 
 public class PassTimeProcessorTests {
     protected Bank bank;
     protected PassTimeProcessor passTimeProcessor;
 
-    protected final String CHECKING_ID = "98430842";
+    protected final String CHECKING_ID = "98408842";
     protected final String SAVINGS_ID = "89438042";
     protected final String CD_ID = "98430842";
     protected final double APR = getMaxAPR();
@@ -41,26 +41,27 @@ public class PassTimeProcessorTests {
         String id = "10000010";
 
         passTimeProcessor.setNext(new CreateProcessor(bank));
-        assertTrue(passTimeProcessor.handle(String.format("create cd %s %f %f", id, APR, INITIAL_CD_BALANCE)));
 
+        assertTrue(passTimeProcessor.handle(String.format("%s %s %s %s %s", TransactionType.Create, AccountType.CD, id, APR, INITIAL_CD_BALANCE)));
         assertEquals(AccountType.CD, bank.getAccount(id).getAccountType());
         assertEquals(id, bank.getAccount(id).getID());
         assertEquals(APR, bank.getAccount(id).getAPR());
         assertEquals(INITIAL_CD_BALANCE, bank.getAccount(id).getBalance());
-        assertFalse(passTimeProcessor.handle(String.format("deposit %s %f", SAVINGS_ID, 2000.0f)));
+        assertFalse(passTimeProcessor.handle(String.format("%s %s %s", TransactionType.Deposit, SAVINGS_ID, Savings.getMaxDepositAmount())));
     }
 
     @Test
     protected void pass_time_transaction_should_apply_apr() {
         double minBalanceFee = bank.getMinBalanceFee();
-        int months = 6;
-        double checkingDepositAmount = 1000;
-        double savingsDepositAmount = 2500;
+        int months = getMonthsPerYear();
+        TransactionType transactionType = TransactionType.PassTime;
+        double checkingDepositAmount = Checking.getMaxDepositAmount();
+        double savingsDepositAmount = Savings.getMaxDepositAmount();
 
         bank.deposit(CHECKING_ID, checkingDepositAmount);
         bank.deposit(SAVINGS_ID, savingsDepositAmount);
-        passTimeProcessor.handle(String.format("pass time %d", months));
 
+        assertTrue(passTimeProcessor.handle(String.format("%s %s", transactionType, months)));
         assertEquals(BankTests.passTime(APR, minBalanceFee, AccountType.Checking, checkingDepositAmount, months), bank.getAccount(CHECKING_ID).getBalance());
         assertEquals(BankTests.passTime(APR, minBalanceFee, AccountType.Savings, savingsDepositAmount, months), bank.getAccount(SAVINGS_ID).getBalance());
         assertEquals(BankTests.passTime(APR, minBalanceFee, AccountType.CD, INITIAL_CD_BALANCE, months), bank.getAccount(CD_ID).getBalance());
@@ -70,13 +71,14 @@ public class PassTimeProcessorTests {
     protected void pass_time_transaction_when_balance_is_less_than_or_equal_to_100_should_apply_min_balance_fee_then_apr() {
         double minBalanceFee = bank.getMinBalanceFee();
         int months = 2;
+        TransactionType transactionType = TransactionType.PassTime;
         double checkingDepositAmount = 75;
         double savingsDepositAmount = 100;
 
         bank.deposit(CHECKING_ID, checkingDepositAmount);
         bank.deposit(SAVINGS_ID, savingsDepositAmount);
-        bank.passTime(months);
 
+        assertTrue(passTimeProcessor.handle(String.format("%s %s", transactionType, months)));
         assertEquals(BankTests.passTime(APR, minBalanceFee, AccountType.Checking, checkingDepositAmount, months), bank.getAccount(CHECKING_ID).getBalance());
         assertEquals(BankTests.passTime(APR, minBalanceFee, AccountType.Savings, savingsDepositAmount, months), bank.getAccount(SAVINGS_ID).getBalance());
         assertEquals(BankTests.passTime(APR, minBalanceFee, AccountType.CD, INITIAL_CD_BALANCE, months), bank.getAccount(CD_ID).getBalance());
@@ -86,10 +88,12 @@ public class PassTimeProcessorTests {
     protected void pass_time_transaction_when_balance_is_0_should_remove_account() {
         double minBalanceFee = bank.getMinBalanceFee();
         int months = 2;
+        TransactionType transactionType = TransactionType.PassTime;
+        double depositAmount = 25;
 
-        bank.deposit(SAVINGS_ID, 25);
-        bank.passTime(months);
+        bank.deposit(SAVINGS_ID, depositAmount);
 
+        assertTrue(passTimeProcessor.handle(String.format("%s %s", transactionType, months)));
         assertFalse(bank.containsAccount(CHECKING_ID));
         assertFalse(bank.containsAccount(SAVINGS_ID));
         assertTrue(bank.containsAccount(CD_ID));
