f8a624cbbe8253e6dd2dad0861febfff9c353064
Michael Tran <solidwater969@gmail.com>
2021-10-06 09:11:37 -0400

    fix: remove mutation in order generator

--- src/main/java/server/game/pushing/paper/order_factory/OrderFactory.java
+++ src/main/java/server/game/pushing/paper/order_factory/OrderFactory.java
@@ -12,24 +12,33 @@ import java.util.List;
 import java.util.Random;
 
 public class OrderFactory {
+    private List<String> order;
+    private List<TransactionFactory> transactionFactories;
+    private ChainOfResponsibility processor;
+
     public List<String> getOrder(int size, Random random) {
-        List<String> order = new ArrayList<>();
+        order = new ArrayList<>();
         Bank bank = new Bank();
-        List<TransactionFactory> transactionFactories = Arrays.asList(new CreateFactory(bank, random), new DepositFactory(bank, random), new WithdrawFactory(bank, random), new TransferFactory(bank, random), new TimeTravelFactory(bank, random));
-        ChainOfResponsibility processor = (new ChainOfResponsibilityFactory(bank)).getChainOfResponsibility(false);
+        transactionFactories = Arrays.asList(new CreateFactory(bank, random), new DepositFactory(bank, random), new WithdrawFactory(bank, random), new TransferFactory(bank, random), new TimeTravelFactory(bank, random));
+        processor = (new ChainOfResponsibilityFactory(bank)).getChainOfResponsibility(false);
 
-        String transaction;
-        for (int i = 0; i < size; i++) {
-            if (i < 2) {
-                transaction = ((CreateFactory) transactionFactories.get(0)).getLoadedTransaction(AccountType.CHECKING);
-            } else {
-                transaction = transactionFactories.get(random.nextInt(transactionFactories.size())).getTransaction();
-            }
-
-            processor.handle(transaction);
-            order.add(transaction);
-        }
+        addAllTransactions(size, random, 2);
 
         return order;
+    }
+
+    private void addAllTransactions(int size, Random random, int weight) {
+        if (weight != 0) {
+            addTransaction(((CreateFactory) transactionFactories.get(0)).getLoadedTransaction(AccountType.CHECKING));
+            addAllTransactions(size - 1, random, weight - 1);
+        } else if (size > 0) {
+            addTransaction(transactionFactories.get(random.nextInt(transactionFactories.size())).getTransaction());
+            addAllTransactions(size - 1, random, 0);
+        }
+    }
+
+    private void addTransaction(String transaction) {
+        processor.handle(transaction);
+        order.add(transaction);
     }
 }
