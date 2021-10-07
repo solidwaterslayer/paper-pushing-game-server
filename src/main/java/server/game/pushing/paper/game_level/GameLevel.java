8b01805a0ca2055d14cd37a71560c29082b09728
Michael Tran <solidwater969@gmail.com>
2021-10-07 11:13:49 -0400

    feature: add transformation in level

--- src/main/java/server/game/pushing/paper/game_level/GameLevel.java
+++ src/main/java/server/game/pushing/paper/game_level/GameLevel.java
@@ -1,37 +1,22 @@
 package server.game.pushing.paper.game_level;
 
+import server.game.pushing.paper.invalid_receipt_factory.InvalidReceiptFactory;
 import server.game.pushing.paper.order_factory.OrderFactory;
-import server.game.pushing.paper.store.Store;
 
 import java.util.List;
 import java.util.Random;
 
 public class GameLevel {
     public List<String> order;
-    public List<String> transformedReceipt;
+    public List<String> invalidReceipt;
     public List<String> receiptTransformation;
 
     public GameLevel() {
         OrderFactory orderFactory = new OrderFactory();
         order = orderFactory.getOrder(9, new Random());
 
-        Store store = new Store();
-        store.getOrder().addAll(order);
-        List<String> receipt = store.getReceipt();
-        transformedReceipt = receipt;
-        receiptTransformation = receipt;
-
-        // change transaction type
-        // change account type
-        // increment id
-        // increment amount
-        // missing transaction [not is empty]
-        // switch 2 transactions
-        // move transaction to invalid
-
-        // amount underload [create and time travel]
-        // amount overload
-        // withdraw from savings twice
-        // withdraw from cd before time traveling 12 months
+        InvalidReceiptFactory invalidReceiptFactory = new InvalidReceiptFactory();
+        invalidReceipt = invalidReceiptFactory.getInvalidReceipt(order);
+        receiptTransformation = invalidReceiptFactory.getReceiptTransformation();
     }
 }
---
+++ src/main/java/server/game/pushing/paper/invalid_receipt_factory/InvalidReceiptFactory.java
@@ -0,0 +1,36 @@
+package server.game.pushing.paper.invalid_receipt_factory;
+
+import server.game.pushing.paper.store.Store;
+
+import java.util.List;
+
+public class InvalidReceiptFactory {
+    public List<String> getInvalidReceipt(List<String> order) {
+        Store store = new Store();
+        store.getOrder().addAll(order);
+        return store.getReceipt();
+        // typo
+            // change transaction type
+            // change account type
+            // increment id
+            // increment amount
+        // move
+        // switch
+            // deposit
+            // withdraw
+            // transfer
+        // remove
+            // deposit
+            // withdraw
+            // transfer
+
+        // overload
+        // paradox
+            // withdraw from savings twice
+            // withdraw from cd before time traveling 12 months
+    }
+
+    public List<String> getReceiptTransformation() {
+        return null;
+    }
+}
