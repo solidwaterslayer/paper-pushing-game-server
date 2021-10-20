6e2568498346830ea19ddf4f151ef7236523d415
Michael Tran <solidwater969@gmail.com>
2021-10-20 05:00:44 -0400

    refactor: move files

--- src/main/java/server/game/pushing/paper/game_level/GameLevel.java
+++ src/main/java/server/game/pushing/paper/game_level/GameLevel.java
@@ -1,22 +1,44 @@
 package server.game.pushing.paper.game_level;
 
-import server.game.pushing.paper.invalid_receipt_generator.InvalidReceiptGenerator;
 import server.game.pushing.paper.order_generator.OrderGenerator;
+import server.game.pushing.paper.store.Store;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 public class GameLevel {
-    public List<String> order;
-    public List<String> invalidReceipt;
-    public List<String> receiptTransformation;
+    private List<String> order;
+    private List<String> receipt;
+    private List<String> transformation;
 
     public GameLevel() {
         OrderGenerator orderGenerator = new OrderGenerator();
         order = orderGenerator.getOrder(9, new Random());
 
-        InvalidReceiptGenerator invalidReceiptGenerator = new InvalidReceiptGenerator();
-        invalidReceipt = invalidReceiptGenerator.getInvalidReceipt(order);
-        receiptTransformation = invalidReceiptGenerator.getReceiptTransformation();
+        Store store = new Store();
+        store.getOrder().addAll(order);
+        receipt = store.getReceipt();
+
+        transformation = new ArrayList<>();
+        // typo
+        // change transaction type
+        // change account type
+        // increment id
+        // increment amount
+        // move
+        // switch
+            // deposit
+            // withdraw
+            // transfer
+        // remove
+            // deposit
+            // withdraw
+            // transfer
+
+        // overload
+        // paradox
+            // withdraw from savings twice
+            // withdraw from cd before time traveling 12 months
     }
 }
