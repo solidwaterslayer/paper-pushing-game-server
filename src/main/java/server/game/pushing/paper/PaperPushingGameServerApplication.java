e09a61405691f89ecc2e4eb33a15dcfb5ccab2f7
Michael Tran <solidwater969@gmail.com>
2022-01-03 15:29:12 -0500

    refactor: remove comments and optimize imports

--- src/main/java/server/game/pushing/paper/PaperPushingGameServerApplication.java
+++ src/main/java/server/game/pushing/paper/PaperPushingGameServerApplication.java
@@ -2,29 +2,10 @@ package server.game.pushing.paper;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-
-import java.util.Arrays;
 
 @SpringBootApplication
 public class PaperPushingGameServerApplication {
     public static void main(String[] arguments) {
         SpringApplication.run(PaperPushingGameServerApplication.class, arguments);
     }
-/*
-
-    @Bean
-    public CorsFilter corsFilter() {
-        CorsConfiguration corsConfiguration = new CorsConfiguration();
-        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
-
-        corsConfiguration.setAllowedOrigins(Arrays.asList("https://solidwaterslayer.github.io", "http://localhost:4200"));
-        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
-
-        return new CorsFilter(urlBasedCorsConfigurationSource);
-    }
-*/
 }
